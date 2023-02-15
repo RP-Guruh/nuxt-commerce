21f621fcb28b0147871b4708ecdfa58a99a7e4aa
RP-Guruh <github.guruh@gmail.com>
2023-02-15 15:39:22 +0700

    fix product card

--- pages/home.vue
+++ pages/home.vue
@@ -23,18 +23,18 @@
 
     <div class="p-4 sm:w-1/2 lg:w-1/3 w-full hover:scale-105 duration-500">
       <div
-        class="flex items-center justify-between p-4 rounded-lg bg-white shadow-indigo-50 shadow-md"
+        class="flex items-center justify-between p-4 rounded-lg bg-white shadow-indigo-50 shadow-xl"
       >
         <div>
-          <h2 class="text-gray-900 text-lg font-bold">Total Ballance</h2>
-          <h3 class="mt-2 text-xl font-bold text-yellow-500 text-left">
-            + 150.000 ₭
-          </h3>
-          <p class="text-sm font-semibold text-gray-400">Last Transaction</p>
+          <h1 class="mt-2 text-2xl font-bold text-yellow-500 text-left">New</h1>
+          <h1 class="text-2xl font-bold text-yellow-500 text-left">Arrival</h1>
+          <p class="pt-4 text-sm font-semibold text-gray-500">
+            With Free Shipping
+          </p>
           <button
-            class="text-sm mt-6 px-4 py-2 bg-yellow-400 text-white rounded-lg tracking-wider hover:bg-yellow-300 outline-none"
+            class="text-sm mt-6 px-4 py-2 font-bold bg-green-600 text-white rounded-lg tracking-wider hover:bg-yellow-300 outline-none"
           >
-            Add to cart
+            Shop Now
           </button>
         </div>
         <div class="flex justify-center items-center">
