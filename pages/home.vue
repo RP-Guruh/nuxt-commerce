3225d194168baa4f5f585c53428b232d02034fb2
RP-Guruh <github.guruh@gmail.com>
2023-02-20 15:17:44 +0700

    test

--- pages/home.vue
+++ pages/home.vue
@@ -43,6 +43,72 @@
       </div>
     </div>
 
+    <div class="px-4">
+      <p>Popular Products</p>
+    </div>
+
+    <article
+      class="rounded-xl w-52 bg-white ml-4 p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300"
+    >
+      <a href="#">
+        <div class="relative flex items-end overflow-hidden rounded-xl">
+          <img
+            src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="Hotel Photo"
+          />
+          <div
+            class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+
+            <button class="text-sm">Add to cart</button>
+          </div>
+        </div>
+
+        <div class="mt-1 p-2">
+          <h2 class="text-slate-700">Adobe Photoshop CC 2022</h2>
+          <p class="mt-1 text-sm text-slate-400">Lisbon, Portugal</p>
+
+          <div class="mt-3 flex items-end justify-between">
+            <p class="text-lg font-bold text-blue-500">$850</p>
+
+            <div
+              class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-4 w-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+
+              <button class="text-sm">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </a>
+    </article>
     <!-- END OF CARD -->
   </div>
 </template>
