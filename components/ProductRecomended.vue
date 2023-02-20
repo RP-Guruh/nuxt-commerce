7fbbe8fa4a0f3e07682e429c8952481b7e5883db
RP-Guruh <github.guruh@gmail.com>
2023-02-20 17:18:51 +0700

    Make component product card

---
+++ components/ProductRecomended.vue
@@ -0,0 +1,27 @@
+<template>
+  <article
+    class="rounded-xl md:w-52 w-40 mt-4 bg-white ml-4 p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300"
+  >
+    <div class="relative flex items-end overflow-hidden rounded-xl">
+      <img
+        src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        alt="Hotel Photo"
+      />
+    </div>
+    <div class="mt-1 p-2">
+      <h2 class="text-slate-700">Adobe Photoshop CC 2022</h2>
+      <p class="mt-1 text-sm text-slate-400">Lisbon, Portugal</p>
+
+      <div class="mt-3 flex items-end justify-between">
+        <p class="text-sm font-bold text-blue-500">$850</p>
+      </div>
+      <div class="mt-3 flex justify-center items-center">
+        <div
+          class="rounded-lg bg-blue-500 px-2 py-1.5 text-white duration-100 hover:bg-blue-600"
+        >
+          <button class="text-sm">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
